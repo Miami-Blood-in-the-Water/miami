7b1b67e987e2d5edafe14558fad7f4da3f34077c
Joe Roberts <jroberts@jroberts.me.uk>
2020-06-01 20:37:13 +0100

    Add race command.

--- character/commands/chargen.py
+++ character/commands/chargen.py
@@ -1,5 +1,25 @@
 from evennia import Command, EvMenu
 
+from typeclasses.characters import Character
+
+
+class CmdRace(Command):
+    key = 'race'
+
+    races = {
+        'character': Character
+    }
+
+    def func(self):
+        args = self.args.strip()
+        try:
+            race = self.races[args]
+            character = race(character=self.caller)
+            character.save()
+            self.msg(f'You are now a {args}.')
+        except ValueError:
+            self.msg(f"No such race, {args}.")
+
 
 class CmdChargen(Command):
     key = "chargen"
--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -16,7 +16,7 @@ own cmdsets by inheriting from them or directly from `evennia.CmdSet`.
 
 from evennia import default_cmds
 
-from character.commands.chargen import CmdChargen
+from character.commands.chargen import CmdChargen, CmdRace
 
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
@@ -34,6 +34,7 @@ class CharacterCmdSet(default_cmds.CharacterCmdSet):
         """
         super().at_cmdset_creation()
         self.add(CmdChargen)
+        self.add(CmdRace)
         #
         # any commands you add below will overload the default ones.
         #
